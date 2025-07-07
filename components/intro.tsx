aa279bc19ad3ff3e68cfc9cf6dc6f5f4ee71ac33
Mehtaraju <123632181+Mehtaraju@users.noreply.github.com>
2025-07-07 11:25:01 +0530

    intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,13 +92,16 @@ export default function Intro() {
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="Raju_Resume.pdf"
-          download
-        >
-          Download CV{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
+  className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 transition duration-300"
+  href="/Raju_Resume.pdf"
+  download
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Download CV{" "}
+  <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+</a>
+
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
